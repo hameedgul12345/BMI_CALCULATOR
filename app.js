7300ef17a05410aadd84e5755bb6135d9604048c
Hameed Gul <hamidkhan31590@gmail.com>
2024-10-08 21:13:24 +0500

    Targeting id's

---
+++ app.js
@@ -0,0 +1,11 @@
+function calculateBMI() {
+    var wg = document.getElementById("wg");
+    var hg = document.getElementById("hg");
+    var bmi_status = document.getElementById("bmi_status");
+    var bmi_h2 = document.getElementById("bmi_h2");
+  
+    console.log(Number(hg.value));
+    console.log(Number(wg.value));
+   
+  }
+  
