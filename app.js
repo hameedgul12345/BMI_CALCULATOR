f6d07dfe8da8673fd89600d11f951cb95d957bd4
Hameed Gul <hamidkhan31590@gmail.com>
2024-10-08 23:24:35 +0500

    Reset Values

--- app.js
+++ app.js
@@ -16,24 +16,41 @@ function calculateBMI() {
   console.log(BMI);
 
   if (BMI < 18.5) {
+    hg.value=""
+    wg.value=""
     bmi_status.textContent = "Underweight";
     bmi_h2.textContent = BMI;
      img_id.src='images/1.png'
     console.log("Underweight");
   } else if (18.5 <= BMI && BMI < 24.9) {
+      hg.value=""
+    wg.value=""
     bmi_status.textContent = "Normal weight";
     bmi_h2.textContent = BMI;
     console.log("Normal weight");
     img_id.src='images/2.png'
   } else if (25 <= BMI && BMI < 29.9) {
+      hg.value=""
+    wg.value=""
     bmi_status.textContent = "Overweight";
     bmi_h2.textContent = BMI;
      img_id.src='images/3.png'
     console.log("Overweight");
   } else {
+      hg.value=""
+    wg.value=""
     bmi_h2.textContent = BMI;
     bmi_status.textContent = "Obesity"
      img_id.src='images/4.png'
     console.log("Obesity");
   }
+}
+function resetDisplay(){
+    var bmi_status = document.getElementById("bmi_status");
+  var bmi_h2 = document.getElementById("bmi_h2");
+  var img_id=document.getElementById('img_id')
+
+  bmi_h2.textContent = '';
+    bmi_status.textContent = ""
+     img_id.src=''
 }
