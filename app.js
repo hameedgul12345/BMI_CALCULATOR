f511daa9e7ca08f399e78e843a7d73a8a1d720c9
Hameed Gul <hamidkhan31590@gmail.com>
2024-10-08 21:18:07 +0500

    Foot to meter Conversion And BMI formula

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 function calculateBMI() {
-    var wg = document.getElementById("wg");
-    var hg = document.getElementById("hg");
-    var bmi_status = document.getElementById("bmi_status");
-    var bmi_h2 = document.getElementById("bmi_h2");
-  
-    console.log(Number(hg.value));
-    console.log(Number(wg.value));
-    if (Number(wg.value) <= 0 || Number(hg.value) <= 0) {
-        return alert("Please Provide a valid Weight Or Height");
-      }
+  var wg = document.getElementById("wg");
+  var hg = document.getElementById("hg");
+  var bmi_status = document.getElementById("bmi_status");
+  var bmi_h2 = document.getElementById("bmi_h2");
+
+  console.log(Number(hg.value));
+  console.log(Number(wg.value));
+  if (Number(wg.value) <= 0 || Number(hg.value) <= 0) {
+    return alert("Please Provide a valid Weight Or Height");
   }
-  
+
+  var meter = Number(hg.value) * 0.3048;
+  var BMI = Number(wg.value) / (meter * meter);
+  console.log(BMI);
+}
