71f2de12eb586949d568cf832a23618d6b896702
Hameed Gul <hamidkhan31590@gmail.com>
2024-10-08 22:46:31 +0500

    Apply if else condition

--- app.js
+++ app.js
@@ -13,4 +13,22 @@ function calculateBMI() {
   var meter = Number(hg.value) * 0.3048;
   var BMI = Number(wg.value) / (meter * meter);
   console.log(BMI);
+
+  if (BMI < 18.5) {
+    bmi_status.textContent = "Underweight";
+    bmi_h2.textContent = BMI;
+    console.log("Underweight");
+  } else if (18.5 <= BMI && BMI < 24.9) {
+    bmi_status.textContent = "Normal weight";
+    bmi_h2.textContent = BMI;
+    console.log("Normal weight");
+  } else if (25 <= BMI && BMI < 29.9) {
+    bmi_status.textContent = "Overweight";
+    bmi_h2.textContent = BMI;
+    console.log("Overweight");
+  } else {
+    bmi_h2.textContent = BMI;
+    bmi_status.textContent = "Obesity"
+    console.log("Obesity");
+  }
 }
