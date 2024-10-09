d2e224718f5390b696cd833ea04c036c4348894f
Hameed Gul <hamidkhan31590@gmail.com>
2024-10-09 10:44:30 +0500

    Apply onload javascript method

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+var bmi_h2 = document.getElementById("bmi_h2");
+window.onload=()=>{
+     img_id.style.display='none';
+     alert('')
+  }
+
+// window.onload(()=>{
+//     // img_id.style.display='none';
+//     alert('')
+// })
 function calculateBMI() {
   var wg = document.getElementById("wg");
   var hg = document.getElementById("hg");
   var bmi_status = document.getElementById("bmi_status");
-  var bmi_h2 = document.getElementById("bmi_h2");
-  var img_id=document.getElementById('img_id')
+
 
   console.log(Number(hg.value));
   console.log(Number(wg.value));
@@ -16,41 +25,45 @@ function calculateBMI() {
   console.log(BMI);
 
   if (BMI < 18.5) {
-    hg.value=""
-    wg.value=""
+    hg.value = "";
+    wg.value = "";
     bmi_status.textContent = "Underweight";
     bmi_h2.textContent = BMI;
-     img_id.src='images/1.png'
+    img_id.style.display='block';
+    img_id.src = "images/1.png";
     console.log("Underweight");
   } else if (18.5 <= BMI && BMI < 24.9) {
-      hg.value=""
-    wg.value=""
+    hg.value = "";
+    wg.value = "";
     bmi_status.textContent = "Normal weight";
     bmi_h2.textContent = BMI;
     console.log("Normal weight");
-    img_id.src='images/2.png'
+    img_id.style.display='block';
+    img_id.src = "images/2.png";
   } else if (25 <= BMI && BMI < 29.9) {
-      hg.value=""
-    wg.value=""
+    hg.value = "";
+    wg.value = "";
     bmi_status.textContent = "Overweight";
     bmi_h2.textContent = BMI;
-     img_id.src='images/3.png'
+    img_id.style.display='block';
+    img_id.src = "images/3.png";
     console.log("Overweight");
   } else {
-      hg.value=""
-    wg.value=""
+    hg.value = "";
+    wg.value = "";
     bmi_h2.textContent = BMI;
-    bmi_status.textContent = "Obesity"
-     img_id.src='images/4.png'
+    bmi_status.textContent = "Obesity";
+    img_id.style.display='block';
+    img_id.src = "images/4.png";
     console.log("Obesity");
   }
 }
-function resetDisplay(){
-    var bmi_status = document.getElementById("bmi_status");
+function resetDisplay() {
+  var bmi_status = document.getElementById("bmi_status");
   var bmi_h2 = document.getElementById("bmi_h2");
-  var img_id=document.getElementById('img_id')
+  var img_id = document.getElementById("img_id");
 
-  bmi_h2.textContent = '';
-    bmi_status.textContent = ""
-     img_id.src=''
+  bmi_h2.textContent = "";
+  bmi_status.textContent = "";
+  img_id.style.display = "none";
 }
